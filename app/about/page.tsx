5aba77efcbf9b17a28d7b70e9158c38f42970097
Ben <bnjox@users.noreply.github.com>
2026-04-02 14:39:54 +0000

    added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti vel
+        mollitia incidunt amet qui illo expedita consectetur quam consequuntur
+        quos aperiam iure necessitatibus nemo nisi eaque sint, fugiat error.
+        Adipisci?
+      </h1>
+    </div>
+  );
+}
